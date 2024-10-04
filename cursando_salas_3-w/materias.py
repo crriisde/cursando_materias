dfc6f34c6c683508c8d895176ebda0af2f03fb18
crriisde <a.23308051281214@cbtis128.edu.mx>
2024-10-03 19:19:38 -0600

    Add files via upload

---
+++ cursando_salas_3-w/materias.py
@@ -0,0 +1,9 @@
+print ("Cristian David Salas De la o 3-w") #Esta linea muestra el nombre del programador
+materias = ["Matematicas", "bioquimica", "Ingles", "Quimica", "Cultura digital", "Cultura metropolitana"] #Esta linea define las materias de la lista
+
+print("Materias del salon") #Esta linea muestra las materias 
+for materia in materias: #Esta linea define la materias de la lista
+    print(materia) #Esta linea muestra las materias
+
+for materia in materias: #Esta linea defina las materias de las lista
+    print(f"Estoy cursando {materia}.")    #Esta linea muestra las materias que estas cursando 
